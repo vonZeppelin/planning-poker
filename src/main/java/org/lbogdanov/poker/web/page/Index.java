3db91f4052ce1313a9500719502e7b470c538e7b
Leonid Bogdanov <leonid_bogdanov@mail.ru>
2012-12-23 13:49:21 +0400

    License info was added

--- src/main/java/org/lbogdanov/poker/web/page/Index.java
+++ src/main/java/org/lbogdanov/poker/web/page/Index.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2012 Leonid Bogdanov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.lbogdanov.poker.web.page;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
@@ -14,6 +29,8 @@ import org.odlabs.wiquery.core.javascript.JsQuery;
 
 
 /**
+ * Represents an index page.
+ * 
  * @author Leonid Bogdanov
  */
 public class Index extends WebPage {
@@ -27,7 +44,7 @@ public class Index extends WebPage {
     }
 
     /**
-     * 
+     * Creates a new instance of <code>Index</code> page.
      */
     public Index() {
         Form<Credentials> internalLogin = new Form<Credentials>("internalLogin", new CompoundPropertyModel<Credentials>(new Credentials()));
@@ -51,6 +68,7 @@ public class Index extends WebPage {
     /**
      * {@inheritDoc}
      */
+    @Override
     public void renderHead(IHeaderResponse response) {
         Bootstrap.renderHeadPlain(response);
     }
